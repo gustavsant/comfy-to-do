e06b2793fee4881722e754446df4d35d1b383f2a
gustavsant <gustavosant3301@gmail.com>
2022-08-05 06:40:25 -0300

    algumas mudanças

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,6 +1,7 @@
 const textArea = document.getElementById('text-area')
 const newNoteBtn = document.querySelector('#new-note-btn')
 const clearBtn = document.querySelector('#clear-btn')
+const grid = document.querySelector('.notes-grid ul')
 
 clearBtn.onclick = function(){
     localStorage.clear()
@@ -10,16 +11,23 @@ let notes = JSON.parse(localStorage.getItem('newNote'))  || []
     
 newNoteBtn.addEventListener('click', () => {
     notes.push(textArea.value)
+    
     localStorage.setItem('newNote', JSON.stringify(notes))
+    BuildNotes(textArea.value)
 })
 
 function LoadCards(){
-    const grid = document.querySelector('.notes-grid')
     for(let c=0 ; c!=notes.length ; c++){
-        let card = document.createElement('li')
-        card.innerText = notes[c]
-        grid.appendChild(card)
+        BuildNotes(notes[c])
     }
     
+}
+function BuildNotes(value){
+    let card = document.createElement('li')
+    let radio = document.createElement('button')
+    radio.innerHTML = '<i class="bi bi-check-lg"></i>'
+    card.innerText = value
+    grid.appendChild(card)
+    card.appendChild(radio)
 }
 LoadCards()
