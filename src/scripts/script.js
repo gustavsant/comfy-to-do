dd5a0421dac383fb3882d7c3e5e78cb3efff3534
gustavsant <gustavosant3301@gmail.com>
2022-08-05 02:39:50 -0300

    primeiro commit, só pra upar no hub

---
+++ src/scripts/script.js
@@ -0,0 +1,26 @@
+const textArea = document.getElementById('text-area')
+const newNoteBtn = document.querySelector('#new-note-btn')
+const clearBtn = document.querySelector('#clear-btn')
+
+clearBtn.onclick = function(){
+    localStorage.clear()
+}
+
+let notes = JSON.parse(localStorage.getItem('newNote'))  || []
+
+newNoteBtn.addEventListener('click', () => {
+    const textArea = document.getElementById('miguel')
+    notes.push(textArea.value)
+    localStorage.setItem('newNote', JSON.stringify(notes))
+})
+
+function LoadCards(){
+    const grid = document.querySelector('.notes-grid')
+    for(let c=0 ; c!=notes.length ; c++){
+        let card = document.createElement('li')
+        card.innerText = notes[c]
+        grid.appendChild(card)
+    }
+    
+}
+LoadCards()
